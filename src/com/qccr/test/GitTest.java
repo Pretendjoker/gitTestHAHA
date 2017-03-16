18c68d3efa5eb502ab1d0c95635c38482576bbfd
nzw <nizewei@qccr.com>
2017-03-16 17:32:35 +0800

    项目的第一次git提交

---
+++ src/com/qccr/test/GitTest.java
@@ -0,0 +1,10 @@
+package com.qccr.test;
+
+/**
+ * Created by Kevin on 2017/3/16.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
